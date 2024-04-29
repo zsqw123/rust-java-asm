79597b2831fd288a928f448fd730067e56ba389c
zsqw123 <zsqw123@vip.qq.com>
2024-04-30 01:25:29 +0800

    done class file basically

--- asm/src/reader/bytes_reader.rs
+++ asm/src/reader/bytes_reader.rs
@@ -7,41 +7,13 @@ pub(crate) trait FromReadContext<T> {
 }
 
 pub(crate) trait UReader {
-    fn read_u8(&self) -> u8;
-    fn read_u16(&self) -> u16;
-    fn read_u32(&self) -> u32;
-    fn read_custom<T: FromReadContext<T>>(&self) -> AsmResult<T>;
+    fn read<T: FromReadContext<T>>(&self) -> AsmResult<T>;
     fn read_vec<T: FromReadContext<T>>(
         &self, vec_size: usize) -> AsmResult<Vec<T>>;
 }
 
 impl<'a> UReader for ReadContext<'a> {
-    fn read_u8(&self) -> u8 {
-        let (bytes, index) = *self;
-        let content = bytes[*index];
-        *index += 1;
-        content
-    }
-
-    fn read_u16(&self) -> u16 {
-        let (bytes, index) = *self;
-        let h = (bytes[*index] as u16) << 8;
-        let l = bytes[*index + 1] as u16;
-        *index += 2;
-        h | l
-    }
-
-    fn read_u32(&self) -> u32 {
-        let (bytes, index) = *self;
-        let a = (bytes[*index] as u32) << 24;
-        let b = (bytes[*index + 1] as u32) << 16;
-        let c = (bytes[*index + 2] as u32) << 8;
-        let d = bytes[*index + 3] as u32;
-        *index += 4;
-        a | b | c | d
-    }
-
-    fn read_custom<T: FromReadContext<T>>(&self) -> AsmResult<T> {
+    fn read<T: FromReadContext<T>>(&self) -> AsmResult<T> {
         T::from_context(self)
     }
 
@@ -50,7 +22,7 @@ impl<'a> UReader for ReadContext<'a> {
         let mut vec = Vec::new();
         vec.reserve(vec_size);
         for i in 0..vec_size {
-            vec[i] = self.read_custom()?;
+            vec[i] = self.read()?;
         }
         Ok(vec)
     }
--- asm/src/reader/jvms_reader.rs
+++ asm/src/reader/jvms_reader.rs
@@ -1,7 +1,7 @@
 use std::io::{BufReader, Read};
 
 use crate::err::{AsmErr, AsmResult};
-use crate::jvms::ClassFile;
+use crate::jvms::{AttributeInfo, ClassFile, CPInfo, FieldInfo, MethodInfo};
 use crate::reader::bytes_reader::{FromReadContext, ReadContext, UReader};
 
 struct JvmsClassReader {}
@@ -22,16 +22,88 @@ impl JvmsClassReader {
 
 impl FromReadContext<ClassFile> for ClassFile {
     fn from_context(context: &ReadContext) -> AsmResult<ClassFile> {
-        ClassFile {
-            magic: context.read_u32(),
-            minor_version: context.read_u16(),
-            major_version: context.read_u16(),
-            constant_pool_count: context.read_u16(),
-            constant_pool: context
-        }
-        Err()
+        let magic: u32 = context.read()?;
+        let minor_version: u16 = context.read()?;
+        let major_version: u16 = context.read()?;
+        let constant_pool_count: u16 = context.read()?;
+        let constant_pool: Vec<CPInfo> = context.read_vec(constant_pool_count as usize)?;
+        let access_flags: u16 = context.read()?;
+        let this_class: u16 = context.read()?;
+        let super_class: u16 = context.read()?;
+        let interfaces_count: u16 = context.read()?;
+        let interfaces: Vec<u16> = context.read_vec::<u16>(interfaces_count as usize)?;
+        let fields_count: u16 = context.read()?;
+        let fields: Vec<FieldInfo> = context.read_vec(fields_count as usize)?;
+        let methods_count: u16 = context.read()?;
+        let methods: Vec<MethodInfo> = context.read_vec(methods_count as usize)?;
+        let attributes_count: u16 = context.read()?;
+        let attributes: Vec<AttributeInfo> = context.read_vec(attributes_count as usize)?;
+        let cf = ClassFile {
+            magic, minor_version, major_version,
+            constant_pool_count, constant_pool,
+            access_flags, this_class, super_class,
+            interfaces_count, interfaces,
+            fields_count, fields,
+            methods_count, methods,
+            attributes_count, attributes,
+        };
+        Ok(cf)
     }
 }
 
+impl FromReadContext<CPInfo> for CPInfo {
+    fn from_context(context: &ReadContext) -> AsmResult<CPInfo> {
+        todo!()
+    }
+}
+
+impl FromReadContext<FieldInfo> for FieldInfo {
+    fn from_context(context: &ReadContext) -> AsmResult<FieldInfo> {
+        todo!()
+    }
+}
+
+impl FromReadContext<MethodInfo> for MethodInfo {
+    fn from_context(context: &ReadContext) -> AsmResult<MethodInfo> {
+        todo!()
+    }
+}
+
+impl FromReadContext<AttributeInfo> for AttributeInfo {
+    fn from_context(context: &ReadContext) -> AsmResult<AttributeInfo> {
+        todo!()
+    }
+}
+
+impl FromReadContext<u8> for u8 {
+    fn from_context(context: &ReadContext) -> AsmResult<u8> {
+        let (bytes, index) = *context;
+        let content = bytes[*index];
+        *index += 1;
+        Ok(content)
+    }
+}
+
+impl FromReadContext<u16> for u16 {
+    fn from_context(context: &ReadContext) -> AsmResult<u16> {
+        let (bytes, index) = *context;
+        let h = (bytes[*index] as u16) << 8;
+        let l = bytes[*index + 1] as u16;
+        *index += 2;
+        Ok(h | l)
+    }
+}
+
+impl FromReadContext<u32> for u32 {
+    fn from_context(context: &ReadContext) -> AsmResult<u32> {
+        let (bytes, index) = *context;
+        let a = (bytes[*index] as u32) << 24;
+        let b = (bytes[*index + 1] as u32) << 16;
+        let c = (bytes[*index + 2] as u32) << 8;
+        let d = bytes[*index + 3] as u32;
+        *index += 4;
+        Ok(a | b | c | d)
+    }
+}
 
 
