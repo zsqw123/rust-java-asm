7da32994a619ab43bc2c2a4a75e9b152fe169bdb
zsqw123 <zsqw123@vip.qq.com>
2024-09-16 18:04:40 +0800

    manual drop ref which borrowed by ref cell

--- asm/src/impls/computable.rs
+++ asm/src/impls/computable.rs
@@ -33,18 +33,20 @@ impl<T, K, V, E> CacheAccessor<K, V, E> for T
     where T: CacheableOwner<K, V, E>,
           K: Clone + Eq + Hash {
     fn get(&self, key: &K) -> ResultRc<V, E> {
-        let mut mut_map_ref = self.cache_map().0.borrow_mut();
-        return if let Some(value) = mut_map_ref.get(key) {
+        let map_ref = self.cache_map().0.borrow();
+        return if let Some(value) = map_ref.get(key) {
             match value.as_ref() {
                 Ok(v) => Ok(Rc::clone(v)),
                 Err(e) => Err(Rc::clone(e)),
             }
         } else {
+            drop(map_ref);
             let computed = match self.compute(key) {
                 Ok(v) => Ok(Rc::new(v)),
                 Err(e) => Err(Rc::new(e)),
             };
             let returned = computed.clone();
+            let mut mut_map_ref = self.cache_map().0.borrow_mut();
             mut_map_ref.insert(key.clone(), computed);
             returned
         }
--- asm/tests/jvms/jvm_read_test.rs
+++ asm/tests/jvms/jvm_read_test.rs
@@ -1,8 +1,21 @@
+use java_asm::err::AsmResult;
+use java_asm::jvms::element::ClassFile;
 use java_asm::jvms::JvmsClassReader;
+use java_asm::node::element::ClassNode;
 
 #[test]
-fn read_jvms() {
+fn read_jvms_test() {
+    println!("{:?}", read_jvms().unwrap());
+}
+
+#[test]
+fn read_node() {
+    let jvms = read_jvms().unwrap();
+    let node = ClassNode::from_jvms(jvms);
+    println!("{:?}", node);
+}
+
+fn read_jvms() -> AsmResult<ClassFile> {
     let bytes = include_bytes!("../res/bytecode/CompileTesting.class");
-    let compile_testing_class = JvmsClassReader::read_class_bytes(bytes).unwrap();
-    println!("{:?}", compile_testing_class);
+    JvmsClassReader::read_class_bytes(bytes)
 }
