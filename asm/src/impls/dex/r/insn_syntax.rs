2729b4ceaa12ba47f4d3d479f79ead383221e5ab
zsqw123 <zsqw123@vip.qq.com>
2024-10-09 02:14:17 +0800

    add all insn syntax

--- asm/src/impls/dex/r/insn_syntax.rs
+++ asm/src/impls/dex/r/insn_syntax.rs
@@ -2,26 +2,120 @@
 
 use crate::dex::Opcode;
 
+macro_rules! syntax {
+    (
+        $(
+            $name:ident {
+                $($field:ident: $t:ty),*
+            };
+        )*
+    ) => {
+        $(
+            #[derive(Copy, Debug, Clone, PartialEq, Eq)]
+            pub struct $name {
+                $(pub $field: $t),*
+            }
+        )*
+    }
+}
+
+#[derive(Copy, Debug, Clone, PartialEq, Eq)]
 pub struct U4(u8);
+#[derive(Copy, Debug, Clone, PartialEq, Eq)]
 pub struct I4(u8);
 
-/// vX -> register
-/// lX -> literal
-/// oX -> address offset
-/// cX -> index of constant pool
-enum InsnSyntax {
-    F00x,
-    F10x(Opcode),
-    F12x { opcode: Opcode, vA: U4, vB: U4 },
-    F11n { opcode: Opcode, vA: U4, lB: I4 },
-    F11x { opcode: Opcode, vA: u8 },
-    F10t { opcode: Opcode, oA: i8 },
-    
-    F20t { opcode: Opcode, oA: i16 },
-    F20bc { opcode: Opcode, vA: u8, cB: u16 },
-    F22x { opcode: Opcode, vA: u8, vB: u16 },
-    F21t { opcode: Opcode, vA: u8, oB: i16 },
-    F21s { opcode: Opcode, vA: u8, lB: i16 },
-    F21h { opcode: Opcode, vA: u8, lB: i16 },
-    F21c { opcode: Opcode, vA: u8, cB: u16 },
+syntax! {
+    F00x {};
+    // XX|op
+    F10x { opcode: Opcode };
+    // B|A|op
+    F12x { opcode: Opcode, vA: U4, vB: U4 };
+    F11n { opcode: Opcode, vA: U4, literalB: I4 };
+    // AA|op
+    F11x { opcode: Opcode, vA: u8 };
+    F10t { opcode: Opcode, offsetA: i8 };
+}
+
+syntax! {
+    // XX|op
+    // AA AA
+    F20t { opcode: Opcode, offsetA: i16 };
+    // AA|op
+    // BB BB
+    F20bc { opcode: Opcode, vA: u8, constB: u16 };
+    // AA|op
+    // BB BB
+    F22x { opcode: Opcode, vA: u8, vB: u16 };
+    F21t { opcode: Opcode, vA: u8, offsetB: i16 };
+    F21s { opcode: Opcode, vA: u8, literalB: i16 };
+    F21h { opcode: Opcode, vA: u8, literalB: i16 };
+    F21c { opcode: Opcode, vA: u8, constB: u16 };
+    // AA|op
+    // CC|BB
+    F23x { opcode: Opcode, vA: u8, vB: u8, vC: u8 };
+    F22b { opcode: Opcode, vA: u8, vB: u8, literalC: i8 };
+    // B|A|op
+    // C C CC
+    F22t { opcode: Opcode, vA: u8, vB: u8, offsetC: i16 };
+    F22s { opcode: Opcode, vA: u8, vB: u8, literalC: i16 };
+    F22c { opcode: Opcode, vA: u8, vB: u8, constC: u16 };
+    F22cs { opcode: Opcode, vA: u8, vB: u8, constC: u16 };
+}
+
+syntax! {
+    // XX|op
+    // AA AA (lo)
+    // AA AA (hi)
+    F30t { opcode: Opcode, offsetA: i32 };
+    // XX|op
+    // AA AA
+    // BB BB
+    F32x { opcode: Opcode, vA: u16, vB: u16 };
+    // AA|op
+    // BB BB (lo)
+    // BB BB (hi)
+    F31i { opcode: Opcode, vA: u8, literalB: i32 };
+    F31t { opcode: Opcode, vA: u8, offsetB: i32 };
+    F31c { opcode: Opcode, vA: u8, constB: u32 };
+    // A|G|op
+    // B B B B
+    // F|E|D|C
+    F35c { opcode: Opcode, vA: U4, vC: U4, vD: U4, vE: U4, vF: U4, vG: U4, constB: u16 };
+    F35ms { opcode: Opcode, vA: U4, vC: U4, vD: U4, vE: U4, vF: U4, vG: U4, constB: u16 };
+    F35mi { opcode: Opcode, vA: U4, vC: U4, vD: U4, vE: U4, vF: U4, vG: U4, constB: u16 };
+    // AA|op
+    // BB|BB
+    // CC|CC
+    //      N = CC CC + AA - 1
+    F3rc { opcode: Opcode, vA: u8, vB: u16, vC: u16, vN: u16 };
+    F3rms { opcode: Opcode, vA: u8, vB: u16, vC: u16, vN: u16 };
+    F3rmi { opcode: Opcode, vA: u8, vB: u16, vC: u16, vN: u16 };
+}
+
+syntax! {
+    // A|G|op
+    // B B B B
+    // F|E|D|C
+    // H H H H
+    F45cc { 
+        opcode: Opcode, vA: U4,
+        vC: U4, vD: U4, vE: U4, vF: U4, vG: U4,
+        constB: u16, constH: u16
+    };
+    // AA|op
+    // BB BB
+    // CC CC
+    // HH HH
+    //      N = CC CC + AA - 1
+    F4rcc { 
+        opcode: Opcode, literalA: u8,
+        constB: u16, constH: u16,
+        vC: u16, vN: u16
+    };
+    // AA|op
+    // BB BB (lo)
+    // BB BB
+    // BB BB
+    // BB BB (hi)
+    F51l { opcode: Opcode, vA: u8, literalB: i64 };
 }
